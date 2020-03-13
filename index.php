f42dface04ff984316b1fa0e2402a701a4282f9c
praveenkumar2200 <48544838+praveenkumar2200@users.noreply.github.com>
2020-03-13 16:36:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title>PHP Mysql REST API CRUD</title>
+  <title>CRUD operations using rest Api</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -10,7 +10,7 @@
   <div class="container">
    <br />
    
-   <h3 align="center">PHP Mysql REST API CRUD</h3>
+   <h3 align="center">CRUD operations</h3>
    <br />
    <div align="right" style="margin-bottom:5px;">
     <button type="button" name="add_button" id="add_button" class="btn btn-success btn-xs">Add</button>
